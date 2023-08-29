e42ccbc476adce04007bc639587df1e8446ad602
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-08-29 16:15:23 -0700

    Initial genmkdn funtion for testing.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -27,13 +27,38 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license || license === 'None') return '';
-  return `## License\nThis project is licensed under the ${license} license. See more details [here]${renderLicenseLink(license)}.`;
+  if (!license || license === "None") return "";
+  return `## License\nThis project is licensed under the ${license} license. See more details [here]${renderLicenseLink(
+    license
+  )}.`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions, you can reach out to me at [${data.github}](https://github.com/${
+    data.github
+  }) or contact me at ${data.email}.
 
 `;
 }
